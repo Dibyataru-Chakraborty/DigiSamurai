ce83746138d8e0420f3c62e278ec99233c97da5d
Dibyataru Chakraborty <dibyataruchakraborty@gmail.com>
2025-04-11 21:32:33 +0530

    Update floating-navbar.js

--- src/components/ui/floating-navbar.js
+++ src/components/ui/floating-navbar.js
@@ -10,7 +10,6 @@ import {
 import Link from "next/link";
 import Image from "next/image";
 import { cn } from "@/lib/utils";
-import { usePathname } from "next/navigation";
 
 // Extracted NavItem component for cleaner code
 
@@ -18,6 +17,7 @@ export const FloatingNav = ({ navItems, className, logo }) => {
   const { scrollYProgress } = useScroll();
   const [visible, setVisible] = useState(true);
   const [hoveredIndex, setHoveredIndex] = useState(null);
+  const [bgTransparent, setBgTransparent] = useState(true);
 
   useMotionValueEvent(scrollYProgress, "change", (current) => {
     if (typeof current === "number") {
@@ -25,6 +25,7 @@ export const FloatingNav = ({ navItems, className, logo }) => {
       let positionY = scrollYProgress.get();
 
       setVisible(positionY > 0 && positionY < 0.3 ? true : direction < 0);
+      setBgTransparent(positionY < 0.1); // Make transparent when at top
     }
   });
 
@@ -39,19 +40,14 @@ export const FloatingNav = ({ navItems, className, logo }) => {
     max-w-screen border border-transparent backdrop-blur-sm
     rounded-xl shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)]
     transition-colors duration-300 font-[Jura]`,
-          className
+          className,
+          bgTransparent ? "bg-white/0" : "bg-white"
         )}
       >
         {/* Left Side: Logo */}
         {logo && (
           <div className="flex items-center justify-start space-x-1 mr-auto">
-            <Image
-              src={logo}
-              alt="Logo"
-              width={120}
-              height={70}
-              className="bg-white rounded-l-lg h-full"
-            />
+            <Image src={logo} alt="Logo" width={120} height={70} />
           </div>
         )}
 
@@ -64,6 +60,7 @@ export const FloatingNav = ({ navItems, className, logo }) => {
               hoveredIndex={hoveredIndex}
               setHoveredIndex={setHoveredIndex}
               idx={index}
+              bgTransparent={bgTransparent}
             />
           ))}
         </div>
@@ -72,24 +69,17 @@ export const FloatingNav = ({ navItems, className, logo }) => {
   );
 };
 
-const NavItem = ({ navItem, setHoveredIndex, idx }) => {
-  const pathname = usePathname();
-  const isActive = pathname === navItem.link;
-
-  return (
-    <Link
-      href={navItem.link}
-      className={cn(
-        "relative inline-flex justify-center items-center group rounded-full h-full w-full hover:text-red-600 transition-colors duration-200 font-[Jura]",
-        isActive ? "text-red-600" : ""
-      )}
-      onMouseEnter={() => setHoveredIndex(idx)}
-      onMouseLeave={() => setHoveredIndex(null)}
-    >
-      <span className="block sm:hidden z-[5100]">{navItem.icon}</span>
-      <span className="hidden text-md sm:block z-[5100] whitespace-nowrap">
-        {navItem.name}
-      </span>
-    </Link>
-  );
-};
+const NavItem = ({ navItem, setHoveredIndex, idx, bgTransparent  }) => (
+  <Link
+    href={navItem.link}
+    className={cn("relative inline-flex justify-center items-center group rounded-full h-full w-full hover:text-primary transition-colors duration-200 font-[Jura]",
+      bgTransparent ? " dark:text-neutral-50" : "")}
+    onMouseEnter={() => setHoveredIndex(idx)}
+    onMouseLeave={() => setHoveredIndex(null)}
+  >
+    <span className="block sm:hidden z-[5100]">{navItem.icon}</span>
+    <span className="hidden text-md sm:block z-[5100] whitespace-nowrap">
+      {navItem.name}
+    </span>
+  </Link>
+);
